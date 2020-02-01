5f7c21094501771dfa739a7b5fbe7353f5dd500b
Joe Sycalik <sycalik@gmail.com>
2020-02-01 11:31:55 -0500

    Working on Pear Movement

---
+++ Pearfect Pair/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float speed = 5.0f;
+
+    void Update()
+    {
+
+        var translation = Input.GetAxis("Horizontal") * speed;
+
+        var rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(Vector3.forward * translation);
+    }
+
+}
